7f63ac86257c95a0287bf6b3dd51077b4acd0fae
shanukun <prathms007@gmail.com>
2020-09-05 01:58:41 +0530

    model: Register models

--- gerver/admin.py
+++ gerver/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Group, Post
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Group)
+admin.site.register(Post)
