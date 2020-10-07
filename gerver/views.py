45cf1e7f5b84c1cef99e9e1fb4f24605c1181a31
shanukun <prathms007@gmail.com>
2020-10-07 18:18:41 +0530

    error: Handle Integrity Error.

--- gerver/views.py
+++ gerver/views.py
@@ -1,3 +1,4 @@
+from django.db.utils import IntegrityError
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -10,7 +11,11 @@ from .serializers import ProfileSerializer
 def register_user(request):
     serializer = ProfileSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        try:
+            serializer.save()
+        except IntegrityError:
+            error = {"status": 500, "msg": "Username already exist."}
+            return Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.validated_data,
                         status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
