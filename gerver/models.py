ef1b73a050dc3fcf1b2bc9c31ac0b814085d8792
shanukun <prathms007@gmail.com>
2020-10-26 18:09:45 +0530

    user model: Add group field to User.

--- gerver/models.py
+++ gerver/models.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
 class Group(models.Model):
     name = models.CharField(max_length=128)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group = models.ManyToManyField(Group)
 
 class Post(models.Model):
     user = models.ForeignKey('Profile', on_delete=models.CASCADE)
     group = models.ForeignKey('Group', on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now_add=True)
     post_text = models.TextField(max_length=512)
-    post_image = models.ImageField(default='default.jpg', upload_to='post_images')
+    post_image = models.ImageField(default='default.jpg',
+                                   upload_to='post_images')
