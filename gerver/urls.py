53a79a7275bf2f8a4acc8833872f67a2bf7f32fb
Shanu <prathms007@gmail.com>
2023-02-16 06:37:21 +0530

    url: Add url for 'accounts' views.

--- gerver/urls.py
+++ gerver/urls.py
@@ -1,7 +1,32 @@
-from django.urls import path, include
-from .import views
+"""gerver URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from .views import home_view
 
 urlpatterns = [
-    path('register/', views.register_user, name='register-user'),
-    path('create_group/', views.create_group, name='create-user'),
+    path("admin/", admin.site.urls),
+    path("", home_view, name="home-view"),
+    path("profiles/", include("profiles.urls", namespace="profiles")),
+    path("posts/", include("posts.urls", namespace="posts")),
+    path("accounts/", include("allauth.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- gerver/views.py
+++ gerver/views.py
@@ -1,47 +1,16 @@
-from gerver.auth import encode
-from .models import Profile
-from .serializers import GroupSerializer, ProfileSerializer
-from django.db.utils import IntegrityError
-from rest_framework import permissions
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
+from django.http import HttpResponse
+from django.shortcuts import render
 
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def register_user(request):
-    serializer = ProfileSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        try:
-            serializer.save()
-        except IntegrityError:
-            error = {"msg": "Username already exist."}
-            return Response(error,
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(encode(serializer.validated_data),
-                        status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,
-                    status=status.HTTP_400_BAD_REQUEST)
+from profiles.models import Profile
 
 
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def create_group(request):
-    serializer = GroupSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        try:
-            serializer.save()
-        except IntegrityError:
-            error = {"status": 500, "msg": "Group already exist."}
-            return Response(error,
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return Response(serializer.validated_data,
-                        status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,
-                    status=status.HTTP_400_BAD_REQUEST)
+def home_view(request):
+    user = request.user
+    hello = "Hello world"
 
-
-@api_view(['POST'])
-@permission_classes((permissions.AllowAny,))
-def authenticate_user(request):
-    pass
+    context = {
+        "user": user,
+        "hello": hello,
+    }
+    return render(request, "main/home.html", context)
+    # return HttpResponse('Hello world')
