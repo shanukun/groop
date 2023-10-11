67cbec8fab54651af19a14c76d462721fcfc252e
Shanu <prathms007@gmail.com>
2023-10-11 17:43:00 +0530

    profile: Improve Profile and Relationship models and update migrations.

--- profiles/models.py
+++ profiles/models.py
@@ -1,31 +1,45 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Q
+from django.db.models import BooleanField, ExpressionWrapper, Q, Subquery
 from django.shortcuts import reverse
-from django.template.defaultfilters import slugify
-
-from .utils import get_random_code
-
-# Create your models here.
 
 
 class ProfileManager(models.Manager):
-    def get_all_profiles_to_invite(self, sender):
-        profiles = Profile.objects.all().exclude(user=sender)
-        profile = Profile.objects.get(user=sender)
-        qs = Relationship.objects.filter(Q(sender=profile) | Q(receiver=profile))
+    def get_all_profiles_to_follow(self, user):
+        my_profile = Profile.objects.get(user=user)
+        following = Relationship.objects.filter(follower=my_profile)
+        profiles = Profile.objects.exclude(
+            Q(id__in=Subquery(following.values("leader"))) | Q(user=user)
+        )
 
-        accepted = set([])
-        for rel in qs:
-            if rel.status == "accepted":
-                accepted.add(rel.receiver)
-                accepted.add(rel.sender)
+        return profiles
 
-        available = [profile for profile in profiles if profile not in accepted]
-        return available
+    def get_followers(self, profile):
+        # profile = Profile.objects.get(user=user)
+        followers = Relationship.objects.filter(leader=profile).values("follower")
+        leaders = Relationship.objects.filter(follower=profile).values("leader")
+
+        followers_profile = Profile.objects.annotate(
+            follows=ExpressionWrapper(
+                Q(pk__in=leaders),
+                output_field=BooleanField(),
+            )
+        ).filter(id__in=followers)
+        return followers_profile
+
+    def get_leaders(self, profile):
+        leaders = Relationship.objects.filter(follower=profile).values("leader")
+
+        followers_profile = Profile.objects.annotate(
+            follows=ExpressionWrapper(
+                Q(pk__in=leaders),
+                output_field=BooleanField(),
+            )
+        ).filter(id__in=leaders)
+        return followers_profile
 
     def get_all_profiles(self, me):
-        profiles = Profile.objects.all().exclude(user=me)
+        profiles = Profile.objects.all().exclude(user=me.user)
         return profiles
 
 
@@ -36,11 +50,10 @@ class Profile(models.Model):
     bio = models.TextField(default="no bio...", max_length=300)
     email = models.EmailField(max_length=200, blank=True)
     country = models.CharField(max_length=200, blank=True)
-    avatar = models.ImageField(default="avatars/avatar.png", upload_to="avatars/")
-    friends = models.ManyToManyField(User, blank=True, related_name="friends")
-    slug = models.SlugField(unique=True, blank=True)
+    avatar = models.ImageField(default="media/avatars/avatar.webp", upload_to="media/avatars/")
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    # is_private = models.BooleanField(default=False)
 
     objects = ProfileManager()
 
@@ -48,66 +61,11 @@ class Profile(models.Model):
         return f"{self.user.username}"
 
     def get_absolute_url(self):
-        return reverse("profiles:profile-detail-view", kwargs={"slug": self.slug})
-
-    def get_all_authors_posts(self):
-        return self.posts.all()
+        return reverse("profiles:profile-detail-view")
 
     @property
-    def get_friends(self):
-        return self.friends.all()
-
-    @property
-    def get_friends_no(self):
-        return self.friends.all().count()
-
-    @property
-    def get_posts_no(self):
-        return self.posts.all().count()
-
-    @property
-    def get_likes_given_no(self):
-        likes = self.like_set.all()
-        total_liked = 0
-        for item in likes:
-            if item.value == "Like":
-                total_liked += 1
-        return total_liked
-
-    @property
-    def get_likes_recieved_no(self):
-        posts = self.posts.all()
-        total_liked = 0
-        for item in posts:
-            total_liked += item.liked.all().count()
-        return total_liked
-
-    __initial_first_name = None
-    __initial_last_name = None
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__initial_first_name = self.first_name
-        self.__initial_last_name = self.last_name
-
-    def save(self, *args, **kwargs):
-        ex = False
-        to_slug = self.slug
-        if (
-            self.first_name != self.__initial_first_name
-            or self.last_name != self.__initial_last_name
-            or self.slug == ""
-        ):
-            if self.first_name and self.last_name:
-                to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
-                ex = Profile.objects.filter(slug=to_slug).exists()
-                while ex:
-                    to_slug = slugify(to_slug + " " + str(get_random_code()))
-                    ex = Profile.objects.filter(slug=to_slug).exists()
-            else:
-                to_slug = str(self.user)
-        self.slug = to_slug
-        super().save(*args, **kwargs)
+    def follower_cnt(self):
+        return Relationship.objects.filter(leader=self).count()
 
 
 # (value for database, what we see in admin page)
@@ -115,16 +73,27 @@ STATUS_CHOICES = (("send", "send"), ("accepted", "accepted"))
 
 
 class RelationshipManager(models.Manager):
-    def invitations_received(self, receiver):
-        qs = Relationship.objects.filter(receiver=receiver, status="send")
+    def follow_unfollow(self, follower, leader):
+        created = False
+        try:
+            Relationship.objects.get(follower=follower, leader=leader).delete()
+        except:
+            Relationship.objects.create(
+                follower=follower, leader=leader, status="accepted"
+            )
+            created = True
+        return created
+
+    def req_received(self, leader):
+        qs = Relationship.objects.filter(leader=leader, status="send")
         return qs
 
 
 class Relationship(models.Model):
-    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="sender")
-    receiver = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, related_name="receiver"
+    follower = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="follower"
     )
+    leader = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="leader")
     status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -132,4 +101,4 @@ class Relationship(models.Model):
     objects = RelationshipManager()
 
     def __str__(self):
-        return f"{self.sender}-{self.receiver}-{self.status}"
+        return f"{self.follower}-{self.leader}-{self.status}"
